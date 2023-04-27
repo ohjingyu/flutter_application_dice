d320574f22c9c8d1e35254d92b48500caa927e3a
ohjingyu <kyeong0644@gmail.com>
2023-04-27 18:09:58 +0900

    main

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Center(
+              child: Text(
+                'Random Dice',
+                style: TextStyle(fontSize: 50),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Center(
+              child: Text(
+                '100',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+          ),
+          Flexible(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      iconSize: 100,
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.play_circle_fill_rounded,
+                      )),
+                  IconButton(
+                      iconSize: 100,
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.check_box_rounded,
+                      )),
+                ],
+              ))
+        ],
+      )),
+    );
+  }
+}
